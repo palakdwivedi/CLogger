737dd2ccc3339ec332773a1a77f48969e88ac726
palakdwivedi <dwivedi.palak238@gmail.com>
2022-02-24 11:09:35 +0530

    git virtual trial

--- C_Logger/Program.cs
+++ C_Logger/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace C_Logger
+namespace myos
 {
-    internal class Program
+    public abstract class OurBaseClass{
+        public abstract void Log(string Messsage);
+    }
+
+    public class OurUser: OurBaseClass{
+
+        private String RecentFile
+        {
+            get;
+            set;
+        }
+
+        private String FileName{
+            get;
+            set;
+        }
+
+        private String FilePath {
+            get;
+            set;
+        }
+
+        public OurUser()
+        {
+            this.RecentFile = Directory.GetCurrentDirectory();
+            this.FileName = "Palak.txt";
+            this.FilePath = this.RecentFile + "/"  + this.FileName;
+        
+        }
+
+        public override void Log(string Messsage)
+        {
+           
+            System.Console.WriteLine("The user is Logged : {0}", Messsage);
+
+            using (System.IO.StreamWriter w = System.IO.File.AppendText(this.FilePath))
+            {
+                w.Write("\r\nLog Entry : ");
+                w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                w.WriteLine("  :{0}", Messsage);
+                w.WriteLine("-----------------------------------------------");
+            }
+        }
+    }
+
+
+
+
+    class Program
     {
         static void Main(string[] args)
         {
+          
+            var logger = new OurUser();
+            logger.Log("Palak Dwivedi");
+
         }
     }
 }
